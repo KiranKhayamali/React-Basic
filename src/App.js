ff80671907c80ca92d909ae01c6cc2967c31e7ae
Kiran Khayamali <kirankhayamali@gmail.com>
2024-08-16 19:48:40 +0545

    Creating components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header(props){
-  return <h1>{props.title}</h1>
+function Header(){
+  return <h1>Hello World</h1>
 }
 function App() {
   return <Header />;
